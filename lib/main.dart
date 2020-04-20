d995355d21a5b9546dab8ee6df7a65da6a9be980
sakamichi-jewel <taigayyy@gmail.com>
2020-04-20 22:51:59 +0900

    add body, text and image

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,22 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.indigo[900],
         appBar: AppBar(
           title: Text('Avocado Club'),
           backgroundColor: Colors.green[600],
+        ),
+        body: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Weocome to Avocado Club!',
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+            ),
+            Image(image: AssetImage('images/avocado.jpg')),
+          ],
         ),
       ),
     ));
